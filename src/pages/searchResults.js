48695275ca6e4a4e3e3b2b800590e46ebc34734d
Michael Foster <fosterm4@rams.colostate.edu>
2020-09-13 16:01:35 -0600

    adds fix for no ranches in state

--- src/pages/searchResults.js
+++ src/pages/searchResults.js
@@ -4,12 +4,23 @@ import Layout from "../components/layout"
 import '../components/searchResults.css'
 import RanchCard from "../components/ranchCard"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+
 var stateClicked
 var upperStateClicked
+let ranchCount = 0
 
-export function stateClick(props){
+export function stateClick(props) {
   upperStateClicked = props;
   stateClicked = props.toLowerCase();
+}
+
+function numRanches(num) {
+  if (num === 0) {
+    return (
+      <h1>No ranches in this state yet...</h1>
+    )
+  }
 }
 
 const SearchResults = ({ data }) => {
@@ -23,27 +34,27 @@ const SearchResults = ({ data }) => {
       <div className="ranchCards">
         {data.allSanityRanch.edges.map(edge => {
           var lowerCaseCompareState = edge.node.state.toLowerCase();
-          if(stateClicked === lowerCaseCompareState){
-          return (
-            <div key={edge.node.slug.current}>
-              <a href = {edge.node.slug.current}>
-                <RanchCard title={edge.node.ranchName} image={edge.node.profileImage.asset.url} description={edge.node.description}></RanchCard>
-              </a>
-            </div>
-          )
-          }
-          else if(stateClicked === undefined || stateClicked === ""){
+          if (stateClicked === lowerCaseCompareState) {
+            ranchCount += 1
             return (
               <div key={edge.node.slug.current}>
-                <a href = {edge.node.slug.current}>
+                <a href={edge.node.slug.current}>
                   <RanchCard title={edge.node.ranchName} image={edge.node.profileImage.asset.url} description={edge.node.description}></RanchCard>
                 </a>
               </div>
             )
-            }
+          }
         })}
       </div>
-
+      <div id="noneFound">
+        {
+          ranchCount < 1 &&
+          <h2>No ranches yet in this state...</h2>
+        }
+      </div>
+      <div id="searchAgain">
+        <Link to="/ranchSearch"><button>Search Another State</button></Link>
+      </div>
     </Layout>
   )
 }
