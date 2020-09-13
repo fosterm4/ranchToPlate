e2ca4cbeff993dfae6c3e9ca91c86b85673bbb6a
Michael Foster <fosterm4@rams.colostate.edu>
2020-09-12 22:45:35 -0600

    fixes contact form styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,26 +1,26 @@
 
 import React, { useState } from "react"
 import '../components/ContactForm.css'
-import {Form, FormGroup,Input, Label, Button} from 'reactstrap'
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap'
 const ContactForm = () => {
 
   const [formState, setFormState] = useState({
     name: "",
-    email:"",
-    phone:"",
-    message:""
+    email: "",
+    phone: "",
+    message: ""
   })
 
   const encode = (data) => {
     return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
   }
 
   const handleChange = e => {
     setFormState({
       ...formState,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
@@ -37,56 +37,58 @@ const ContactForm = () => {
   }
 
 
-    return (
-        <Form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-          <div className="wholeForm">
-            <FormGroup>
-              <Input type="hidden" name="form-name" value="contact" />
-              <Label htmlFor="name">Name</Label>
-              <Input 
-              id="name" 
-              name="name"
-              type="text" 
-              onChange={handleChange} 
-              value={formState.name} 
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="email">Email</Label>
-              <Input 
-              id="email" 
-              name="email"
-              type="email" 
-              onChange={handleChange} 
-              value={formState.email} 
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="phone">Phone#</Label>
-              <Input 
-              id="phone" 
-              name="phone"
-              type="text" 
-              onChange={handleChange} 
-              value={formState.phone} 
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label htmlFor="message">Message</Label>
-              <Input 
-              id="message" 
-              name="message"
-              type="textarea" 
-              onChange={handleChange} 
-              value={formState.message} 
-              />
-            </FormGroup>
-            <Button type="submit">Submit</Button>
-          </div>
-        </Form>
-    );
+  return (
+    <Form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <div className="wholeForm">
+        <FormGroup id="name">
+          <Input type="hidden" name="form-name" value="contact" />
+          <Label htmlFor="name">Name</Label>
+          <Input
+            id="name"
+            name="name"
+            type="text"
+            onChange={handleChange}
+            value={formState.name}
+          />
+        </FormGroup>
+        <FormGroup id="email">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            onChange={handleChange}
+            value={formState.email}
+          />
+        </FormGroup>
+        <FormGroup id="phone">
+          <Label htmlFor="phone">Phone#</Label>
+          <Input
+            id="phone"
+            name="phone"
+            type="text"
+            onChange={handleChange}
+            value={formState.phone}
+          />
+        </FormGroup>
+        <FormGroup id="textArea">
+          <Label htmlFor="message">Message</Label>
+          <Input
+            id="message"
+            name="message"
+            type="textarea"
+            onChange={handleChange}
+            value={formState.message}
+          />
+        </FormGroup>
+        <div id = "button">
+          <Button type="submit">Submit</Button>
+        </div>
+      </div>
+    </Form>
+  );
 
-  }
+}
 
 
 export default ContactForm
