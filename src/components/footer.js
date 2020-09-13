172204469e7f6b1a45846ea30b8a8e2fc7b9b6e9
Michael Foster <fosterm4@rams.colostate.edu>
2020-09-13 12:58:52 -0600

    adds social icons to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,16 +2,27 @@ import React from "react";
 import "./Footer.css"
 import { Link } from "gatsby"
 import facebook from "../images/facebook.png"
+import instagram from "../images/insta.png"
+import youtube from "../images/youtube.png"
+
 function Footer() {
   return (
     <div className = "mainFooter">
       <p> Questions? <Link to="/contact">Contact us</Link>
       </p>
       <div className = "socialIcons">
-
+        <a target="_blank" href="https://www.facebook.com/Ranch-to-Plate-101775518347698/">
+          <img src = {facebook} width = "30px" alt = "facebook"></img>
+        </a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCJwyCaWMxNht7F2qky4W9gA?view_as=subscriber">
+          <img src = {youtube} width = "30px" alt = "youtube"></img>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/ranchtoplate/?hl=en">
+          <img src = {instagram} width = "30px" alt = "instagram"></img>
+        </a>
       </div>
       < p className = "copyright">
-        &copy;{new Date().getFullYear()} www.ranchtoplate.com
+        &copy;{new Date().getFullYear()} www.Ranchtoplate.com
       </p>
     </div>
   );
