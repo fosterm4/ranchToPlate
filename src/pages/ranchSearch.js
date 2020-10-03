85cf986f52bc460c8ebbd6f8e685790118637517
Michael Foster <fosterm4@rams.colostate.edu>
2020-10-02 22:24:23 -0600

    aboutus background optimized

--- src/pages/ranchSearch.js
+++ src/pages/ranchSearch.js
@@ -1,26 +1,46 @@
 import React, { Component } from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import '../components/ranchSearch.css'
 import StateMap from '../components/stateMap'
 import SEO from "../components/seo"
-class RanchSearch extends Component {
-  render() {
-    return (
-      <Layout>
-        <SEO title="Search" />
-        <div className="background" />
-        <div className="formSpace">
-          <h1>Select your state below</h1>
-          <Link to="/searchResults">
-            <StateMap></StateMap>
-          </Link>
-        </div>
-      </Layout>
-    );
-  }
-}
+import { StaticQuery, graphql, Link } from "gatsby"
+import BackgroundImage from 'gatsby-background-image'
 
-
+const RanchSearch = () => (
+  <StaticQuery
+    query={graphql`
+    query {
+      desktop: file(relativePath: { eq: "LetTherBeMeat.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `}
+    render={data => {
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <Layout>
+          <SEO title="Search" />
+          <div className="background">
+            <BackgroundImage
+              className="background"
+              fluid={imageData}
+              backgroundColor={`#040e18`}
+            ></BackgroundImage>
+          </div>
+          <div className="formSpace">
+            <h1>Select your state below</h1>
+            <Link to="/searchResults">
+              <StateMap></StateMap>
+            </Link>
+          </div>
+        </Layout>
+      )
+    }}
+  />
+)
 
 export default RanchSearch
